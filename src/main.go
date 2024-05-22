049605d78da30e769c0d823b388d4b7532f8643c
Alcir Junior <junior@cajucomunica.com.br>
2024-05-21 21:24:21 -0300

    chore: :sparkles: add dup examples

--- src/main.go
+++ src/main.go
@@ -3,8 +3,11 @@ package main
 import "github.com/alcir-junior-caju/study-go-livro-a-linguagem-de-programacao-go/src/tutorials"
 
 func main() {
-	tutorials.ShowHelloWorld()
-	tutorials.ShowEcho1()
-	tutorials.ShowEcho2()
-	tutorials.ShowEcho3()
+	// tutorials.ShowHelloWorld()
+	// tutorials.ShowEcho1()
+	// tutorials.ShowEcho2()
+	// tutorials.ShowEcho3()
+	// tutorials.ShowDup1()
+	// tutorials.ShowDup2()
+	tutorials.ShowDup3()
 }
